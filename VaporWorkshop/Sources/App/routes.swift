fb1337c3e0bb5f660ec67148d189d87bcd16fab1
Arminas Ruzgas <arminas.ruzgas@revelsystems.com>
2019-07-17 16:43:08 +0300

    Update routes.swift

--- VaporWorkshop/Sources/App/routes.swift
+++ VaporWorkshop/Sources/App/routes.swift
@@ -3,6 +3,11 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     //MARK: Simple request examples
+        router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        
+        return "Hello, \(name)!"
+        }
     
     //MARK: CRUD database operations examples
 }
